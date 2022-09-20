31dae5b9454f0a15670f0b5a308f7df807c4b8fd
SacredStar <shmelevka93@yandex.ru>
2022-09-20 13:55:22 +0300

    Hash and Key functions examples added

---
+++ GostCrypto/Examples/HashFrunctionsExample.go
@@ -0,0 +1,232 @@
+package Examples
+
+import (
+	"fmt"
+	"github.com/SacredStar/Encryption.go/GostCrypto"
+	win32 "github.com/SacredStar/Encryption.go/GostCrypto/Win32GoFunctions"
+	"os"
+	"syscall"
+)
+
+// CreateHashExample Пример на создание хеша
+func CreateHashExample() {
+	//Вызываем контекст провайдера используя высокоуровневую функцию библиотеки, она соответствует CryptAccquireContext
+	//Далее опущены адекватные проверки функций DestroyHash,ReleaseContext, в проде должны быть обработаны.
+	gost, err := GostCrypto.NewGostCrypto(nil, nil, win32.ProvGost2012, win32.CRYPT_VERIFYCONTEXT)
+	if err != nil {
+		fmt.Printf("Error Accuiqre context")
+		return
+	}
+	//Создадим аналог файла открытого в байтовом формате, для простоты можно использовать слайс байт
+	file := []byte("Test Example")
+	//--------------------------------------------------------------------
+	// Создание пустого объекта функции хэширования.Так как функция принимает не указатель на провайдер, используем дереференс
+	if err := win32.CryptCreateHash(*gost.GetPtrToProviderHandle(), win32.CALG_GR3411_2012_256, 0, gost.GetPtrToHashHandle()); err != nil {
+		fmt.Println("ERROR:%s", err.Error())
+		fmt.Printf("ERROR:%s", err.Error())
+		if win32.CryptDestroyHash(*gost.GetPtrToHashHandle()) != nil {
+			fmt.Printf("ERROR:%s\n", err.Error())
+		}
+		if win32.CryptReleaseContext(*gost.GetPtrToProviderHandle()) != nil {
+			fmt.Printf("ERROR:%s", err.Error())
+		}
+		return
+	}
+	//--------------------------------------------------------------------
+	// Чтение данных из файла и хэширование этих данных. Поскольку все win функции принимают внутри себя разные виды переменных, могут требоваться дополнительные преобразования
+	// в частности, тут требуется приведение типов к *byte и uint32
+	if err := win32.CryptHashData(*gost.GetPtrToHashHandle(), &file[0], uint32(len(file)), 0); err != nil {
+		fmt.Printf("ERROR:%s", err.Error())
+		if win32.CryptDestroyHash(*gost.GetPtrToHashHandle()) != nil {
+			fmt.Printf("ERROR:%s\n", err.Error())
+		}
+		if win32.CryptReleaseContext(*gost.GetPtrToProviderHandle()) != nil {
+			fmt.Printf("ERROR:%s", err.Error())
+		}
+		return
+	}
+	//--------------------------------------------------------------------
+	// Получение параметра объекта функции хэширования. size - размер полученного хеш-значения в байтах.Устанавливается в зависимости от алгоритма.
+	size := 32
+	var resultHash = make([]byte, size)
+	pdwDataLen := uint32(size)
+	if err := win32.CryptGetHashParam(
+		*gost.GetPtrToHashHandle(),
+		win32.HP_HASHVAL,
+		&resultHash[0],
+		&pdwDataLen,
+		0); err != nil {
+		fmt.Printf("ERROR:%s", err.Error())
+		if win32.CryptDestroyHash(*gost.GetPtrToHashHandle()) != nil {
+			fmt.Printf("ERROR:%s\n", err.Error())
+		}
+		if win32.CryptReleaseContext(*gost.GetPtrToProviderHandle()) != nil {
+			fmt.Printf("ERROR:%s", err.Error())
+		}
+		return
+	}
+
+	fmt.Printf("%X", resultHash)
+	// Освобождаем ресурсы
+	if err := win32.CryptDestroyHash(*gost.GetPtrToHashHandle()); err != nil {
+		fmt.Printf("ERROR:%s\n", err.Error())
+	}
+	if err := win32.CryptReleaseContext(*gost.GetPtrToProviderHandle()); err != nil {
+		fmt.Printf("ERROR:%s", err.Error())
+	}
+
+}
+
+// CreateSignExample Пример на подпись объекта функции хэширования и проверку подписи
+func CreateSignExample() (err error) {
+	pbBuffer := []byte("Data to be hashed and signed")
+	dwBufferLen := len(pbBuffer)
+	Container, err := syscall.UTF16PtrFromString("user")
+	if err != nil {
+		fmt.Printf("error get ptr from string")
+		return err
+	}
+	//CryptAcquireContext
+	gost, err := GostCrypto.NewGostCrypto(Container, nil, win32.ProvGost2012, 0)
+	if err != nil {
+		return err
+	}
+	// не забываем уничтожить хендлы...
+	defer win32.CryptDestroyHash(*gost.GetPtrToHashHandle())
+	defer win32.CryptReleaseContext(*gost.GetPtrToProviderHandle())
+	// Получение открытого ключа подписи. Этот открытый ключ будет
+	// использоваться получателем хэша для проверки подписи.
+	// В случае, когда получатель имеет доступ к открытому ключю
+	// отправителя с помощью сертификата, этот шаг не нужен.
+	var phKey win32.Handle
+	if err := win32.CryptGetUserKey(*gost.GetPtrToProviderHandle(), win32.AT_SIGNATURE, &phKey); err != nil {
+		fmt.Printf("error CryptGetUserKey:%s\n", err.Error())
+		return nil
+	}
+	//CryptGetUserKey(
+	//        hProv,
+	//        AT_SIGNATURE,
+	//        &hKey))
+	if err = win32.CryptGetUserKey(*gost.GetPtrToProviderHandle(), win32.AT_SIGNATURE, &phKey); err != nil {
+		fmt.Printf("error CryptGetUserKey:%s\n", err.Error())
+		return nil
+	}
+	// Экпорт открытого ключа. Здесь открытый ключ экспортируется в
+	// PUBLICKEYBOLB для того, чтобы получатель подписанного хэша мог
+	// проверить подпись. Этот BLOB может быть записан в файл и передан
+	// другому пользователю.
+
+	//CryptExportKey(
+	//        hKey,
+	//        0,
+	//        PUBLICKEYBLOB,
+	//        0,
+	//        NULL,
+	//        &dwBlobLen))
+	var dwBlobLen uint32
+	if err = win32.CryptExportKey(phKey, 0, win32.PUBLICKEYBLOB, 0, nil, &dwBlobLen); err != nil {
+		fmt.Printf("error CryptExportKey,get len Error:%s\n", err.Error())
+		return nil
+	}
+	keyBlob := make([]byte, dwBlobLen)
+	if err = win32.CryptExportKey(phKey, 0, win32.PUBLICKEYBLOB, 0, &keyBlob[0], &dwBlobLen); err != nil {
+		fmt.Printf("error CryptExportKey,get key Error:%s\n", err.Error())
+		return nil
+	}
+
+	// Создание объекта функции хэширования.
+	err = win32.CryptCreateHash(*gost.GetPtrToProviderHandle(), win32.CALG_GR3411_2012_256, 0, gost.GetPtrToHashHandle())
+	if err != nil {
+		fmt.Printf("error CryptCreateHash Error:%s\n", err.Error())
+		return nil
+	}
+
+	//Определение размера BLOBа и распределение памяти.
+	//TODO:Check this
+	var cbHash uint32
+	if err = win32.CryptGetHashParam(*gost.GetPtrToHashHandle(), win32.HP_OID, nil, &cbHash, 0); err != nil {
+		fmt.Printf("error CryptGetHashParam Error:%s\n", err.Error())
+		return err
+	}
+	cbHash = 256
+	var pbHash = make([]byte, cbHash)
+	if err = win32.CryptGetHashParam(*gost.GetPtrToHashHandle(), win32.HP_OID, &pbHash[0], &cbHash, 0); err != nil {
+		fmt.Printf("error CryptGetHashParam Error:%s\n", err.Error())
+		return err
+	}
+
+	// Вычисление криптографического хэша буфера.
+	if err = win32.CryptHashData(*gost.GetPtrToHashHandle(), &pbBuffer[0], uint32(dwBufferLen), 0); err != nil {
+		fmt.Printf("error CryptHashData Error:%s\n", err.Error())
+		return err
+	}
+	//Определение размера подписи и распределение памяти.
+	var dwSigLen uint32
+
+	if err = win32.CryptSignHash(*gost.GetPtrToHashHandle(), win32.AT_SIGNATURE, nil, 0, nil, &dwSigLen); err != nil {
+		fmt.Printf("error CryptSignHash Error:%s\n", err.Error())
+		return err
+	}
+	pbSignature := make([]byte, dwSigLen)
+
+	if err = win32.CryptSignHash(*gost.GetPtrToHashHandle(), win32.AT_SIGNATURE, nil, 0, &pbSignature[0], &dwSigLen); err != nil {
+		fmt.Printf("error CryptSignHash Error:%s\n", err.Error())
+		return err
+	}
+	// Запись в файл
+	file, err := os.Create("./SignatureHashFunctionExample.txt")
+	if err != nil {
+		fmt.Printf("error CreateFile Error:%s\n", err.Error())
+		return err
+	}
+
+	file.Write(pbSignature)
+	file.Close()
+	fmt.Printf("End of 1st stage of programm...\n")
+
+	// Во второй части программы проверяется подпись.
+	// Чаще всего проверка осуществляется в случае, когда различные
+	// пользователи используют одну и ту же программу. Хэш, подпись,
+	// а также PUBLICKEYBLOB могут быть прочитаны из файла, e-mail сообщения
+	// или из другого источника.
+
+	// Здесь используюся определенные ранее pbBuffer, pbSignature,
+	// szDescription, pbKeyBlob и их длины.
+
+	// Содержимое буфера pbBuffer представляет из себя некоторые
+	// подписанные ранее данные.
+
+	// Указатель szDescription на текст, описывающий данные, подписывается.
+	// Это тот же самый текст описания, который был ранее передан
+	// функции CryptSignHash.
+
+	//--------------------------------------------------------------------
+	// Получение откытого ключа пользователя, который создал цифровую подпись,
+	// и импортирование его в CSP с помощью функции CryptImportKey. Она
+	// возвращает дескриптор открытого ключа в hPubKey.
+	var hPubKey win32.Handle
+	if err = win32.CryptImportKey(*gost.GetPtrToProviderHandle(), &keyBlob[0], dwBlobLen, 0, 0, &hPubKey); err != nil {
+		fmt.Printf("Error CryptImportKey:%s", err.Error())
+	}
+
+	// Создание объекта функции хэширования.
+	err = win32.CryptCreateHash(*gost.GetPtrToProviderHandle(), win32.CALG_GR3411_2012_256, 0, gost.GetPtrToHashHandle())
+	if err != nil {
+		fmt.Printf("error CryptCreateHash Error:%s\n", err.Error())
+		return nil
+	}
+	//--------------------------------------------------------------------
+	// Вычисление криптографического хэша буфера.
+	if err = win32.CryptHashData(*gost.GetPtrToHashHandle(), &pbBuffer[0], uint32(dwBufferLen), 0); err != nil {
+		fmt.Printf("error CryptHashData Error:%s\n", err.Error())
+		return err
+	}
+	if err = win32.CryptVerifySignature(*gost.GetPtrToHashHandle(), &pbSignature[0], dwSigLen, hPubKey, nil, 0); err != nil {
+		fmt.Printf("error CryptVerifySignature Error:%s\n", err.Error())
+		return err
+	} else {
+		fmt.Printf("Signature is good. End programm.")
+	}
+
+	return nil
+}
---
+++ GostCrypto/Examples/KeyProcessFunctionsExample.go
@@ -0,0 +1,64 @@
+package Examples
+
+import (
+	"fmt"
+	"github.com/SacredStar/Encryption.go/GostCrypto"
+	win32 "github.com/SacredStar/Encryption.go/GostCrypto/Win32GoFunctions"
+	"syscall"
+)
+
+//--------------------------------------------------------------------
+// Пример создания ключевого контейнера с именем по умолчанию.
+//В контейнере созданиются два ключа (ключ обмена и ключ подписи).
+
+func KeyGenExample() (err error) {
+	//Создание ключевой пары
+	//Задается имя ключевого контейнера
+
+	Container, err := syscall.UTF16PtrFromString("user")
+	if err != nil {
+		fmt.Printf("error get ptr from string")
+		return err
+	}
+	gost, err := GostCrypto.NewGostCrypto(Container, nil, win32.ProvGost2012, 0)
+	if err != nil {
+		gost, err = GostCrypto.NewGostCrypto(Container, nil, win32.ProvGost2012, win32.CRYPT_NEWKEYSET)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Криптографический контекст с ключевым контейнером доступен. Получение
+	// имени ключевого контейнера.
+	var dwUserNameLen uint32
+	if err = win32.CryptGetProvParam(*gost.GetPtrToProviderHandle(), win32.PP_CONTAINER, nil, &dwUserNameLen, 0); err != nil {
+		fmt.Printf("error CryptGetProvParam:%s\n", err.Error())
+		return nil
+	}
+	pszUserName := make([]byte, dwUserNameLen)
+	if err = win32.CryptGetProvParam(*gost.GetPtrToProviderHandle(), win32.PP_CONTAINER, &pszUserName[0], &dwUserNameLen, 0); err != nil {
+		fmt.Printf("error CryptGetProvParam:%s\n", err.Error())
+		return nil
+	}
+	fmt.Printf("Get Provider Param returned container:%s", pszUserName)
+
+	var hKey win32.Handle
+	if err = win32.CryptGetUserKey(*gost.GetPtrToProviderHandle(), win32.AT_SIGNATURE, &hKey); err != nil {
+		if err == syscall.Errno(win32.NTE_NO_KEY) {
+			// Создание подписанной ключевой пары.
+			fmt.Println("The signature key does not exist.")
+			fmt.Println("Creating a signature key pair...")
+			if err = win32.CryptGenKey(*gost.GetPtrToProviderHandle(), win32.AT_SIGNATURE, 0, &hKey); err != nil {
+				fmt.Printf("error CryptGenKey:%s\n", err.Error())
+				return nil
+			}
+		}
+	}
+
+	// Получение ключа обмена: AT_KEYEXCHANGE
+	if err = win32.CryptGetUserKey(*gost.GetPtrToProviderHandle(), win32.AT_SIGNATURE, &hKey); err != nil {
+		//printf("No exchange key is available.\n");
+		fmt.Printf("error CryptGetUserKey:%s\n", err.Error())
+	}
+	return nil
+}
